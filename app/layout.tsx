012c94614313729d182f0072660adb5077b853bb
Unrealfocus <emmanuelkefas16@gmail.com>
2025-05-12 02:13:16 +0100

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,11 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: 'Jikonu - Digital Marketing, SEO & Web Development Services',
-  description: 'Grow your brand with Jikonu. We offer expert social media marketing, SEO, ads campaigns, and website development tailored to your goals.',
-  keywords: 'Jikonu, digital marketing, SEO services, social media marketing, website development, Google Ads, Facebook Ads, Instagram marketing',
+  description: 'Grow your brand with Jikonu. We offer expert social media marketing, SEO, ads campaigns, and website development tailored to your goals. Expert Digital Marketing for Growth · Our Services · Social Media Marketing · SEO & Ads Campaigns · Website Building & Management · Why Thousands of. Businesses ...',
+  keywords: 'Jikonu,jikonu.com,jikonu, digital marketing, SEO services, social media marketing, website development, Google Ads, Facebook Ads, Instagram marketing ',
   openGraph: {
     title: 'Jikonu Digital Services',
-    description: 'Expert services in social media marketing, SEO, ads, and web development.',
+    description: 'Expert services in social media marketing, SEO, ads, and web development. Expert Digital Marketing for Growth · Our Services · Social Media Marketing · SEO & Ads Campaigns · Website Building & Management · Why Thousands of. Businesses ...',
     url: 'https://www.jikonu.com/',
     images: [
       {
